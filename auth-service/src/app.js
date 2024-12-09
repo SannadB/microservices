3ecdf76326cf38f7dc47113dba08d301a2bfbbf4
Sannad <sannad98@gmail.com>
2024-12-09 15:19:06 +0500

    ccors fixes

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -4,10 +4,19 @@ const { initDb } = require('./config/database');
 const { connectRabbitMQ } = require('./config/rabbitmq');
 const authRoutes = require('./routes/authRoutes');
 const errorMiddleware = require('./middlewares/errorMiddleware');
+const cors = require('cors'); // Import cors
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
--- order-service/src/app.py
+++ order-service/src/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config.database import get_database
 from routes.order_routes import create_order_routes
 from dotenv import load_dotenv
+from flask_cors import CORS, cross_origin
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, support_credentials=True)
 
     # Connect to MongoDB
     db = get_database()
